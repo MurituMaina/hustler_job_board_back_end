698254f027d8365ca964406c0a36c76bfb2d8951
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-12-05 15:23:13 +0300

    add jobs validations

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,10 @@
 class Job < ApplicationRecord
     belongs_to :recruiter
+
+#     NON_VALIDATABLE_ATTRS = ["id", "created_at", "updated_at"] #or any other attribute that does not need validation
+#     VALIDATABLE_ATTRS = Job.attribute_names.reject{|attr| NON_VALIDATABLE_ATTRS.include?(attr)}
+
+#   validates_presence_of VALIDATABLE_ATTRS
+validates :company_name, :title, :description, :requirements, presence: true
+
 end
