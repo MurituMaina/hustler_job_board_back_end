8f6b41c70e8cf5b2c12a30c05e21260ad9fb768a
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-12-06 11:18:38 +0300

    update CreateJobs migration

--- db/migrate/20221205094602_create_recruiters.rb
+++ db/migrate/20221205094602_create_recruiters.rb
@@ -1,7 +1,7 @@
 class CreateRecruiters < ActiveRecord::Migration[7.0]
   def change
     create_table :recruiters do |t|
-      t.string :name
+      t.string :email
       t.string :password_digest
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_094602) do
   end
 
   create_table "recruiters", force: :cascade do |t|
-    t.string "name"
+    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
